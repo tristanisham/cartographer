e939cfc3ecff1ecdf8135f379dbc21abdbf01e4c
Tristan Isham <tristan@isham.co>
2022-03-26 00:08:29 -0400

    switched to premake for reliability and portability

---
+++ premake5.lua
@@ -0,0 +1,19 @@
+workspace "HelloWorld"
+	configurations { "Debug", "Release" }
+
+project "Cartographer"
+	kind "StaticLib"
+	language "C++"
+	targetdir "build/%{cfg.buildcfg}"
+	objdir ("build/obj")
+	cppdialect "C++20"
+
+	files { "src/**.hpp", "src/**.cpp" }
+
+	filter "configurations:Debug"
+		defines { "DEBUG" }
+		symbols "On"
+
+	filter "configurations:Release"
+		defines { "NDEGUG" }
+		optimize "On"
